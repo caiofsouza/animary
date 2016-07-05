ef1d4888fafbb6d84a7e58386aae70d57cc26a3b
Caio Fernandes <caio_fsouza@hotmail.com>
2016-07-05 19:07:38 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+'use strict';
+// Animation Gif Library
+// Animary
+var Animary = function(){
+
+
+
+	this.triangleAnimation = function(elem){
+		this.addClass(elem+'.left-triangle', 'animary-triangle-circle-left');
+		this.addClass(elem+'.right-triangle', 'animary-triangle-circle-right');
+	};
+
+
+	this.batteryBar = function(elem){
+		var bar = document.createElement('div');
+		bar.className = 'animary-bar-animary';
+		document.querySelector(elem).appendChild(bar);
+	};
+
+	this.circleTunning = function(elem){
+		this.addClass(elem, 'animary-circle-tunning');
+	}
+
+
+
+
+
+	// HELPER FUNCTIONS
+	this.getClasses = function(elem){
+		return document.querySelector(elem).className;
+	}
+
+	this.addClass = function(elem, new_classes){
+		var current_classes = this.getClasses(elem);
+		var formatted_classes = new_classes.replace(/\s/gi, '');
+		document.querySelector(elem).className = current_classes + ' ' + formatted_classes;
+	}
+
+
+	return this; 
+};
+
+
+
+
